53c73fed8ac6e12362e27e591f4681e8e22d891c
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-07-06 19:04:20 -0400

    Get by id is functional and passing tests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -17,4 +17,25 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    Post.findById(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({
+                    message: 'The post with the specified ID does not exist'
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'The post information could not be retrieved',
+                error: err.message
+            })
+        })
+
+})
+
 module.exports = router
