055b5ad2b746118382413baec9747a3644aaab78
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-07-06 18:29:46 -0400

    PostsRouter is connected and get all posts is functional

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,14 @@
-// implement your posts router here
+const express = require('express')
+
+const Post = require('./posts-model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Post.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const postsRouter = require('./posts/posts-router')
 
 const server = express()
 
 server.use(express.json())
+
+server.use('/api/posts', postsRouter)
 
 module.exports = server
